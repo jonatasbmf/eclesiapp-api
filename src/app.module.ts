e2101748c5fe6dba78eb519e6af87efa74abbb9b
Jônatas Freitas - RogueTwo <jonatasbmf@gmail.com>
2026-03-04 22:36:34 -0300

    Organizaçao estrutural inicial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './lib/prisma.service';
 import { ConfigModule } from '@nestjs/config';
-import { PostModule } from './post/post.module';
+import { HealthModule } from './health/health.module';
+import { OrganizationModule } from './module/organization/organization.module';
+import { AuthModule } from './module/auth/auth.module';
+import { UserModule } from './module/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // disponível em toda aplicação
       envFilePath: '.env', // caminho para o arquivo .env
-    }),PostModule],
+    }), HealthModule, OrganizationModule, AuthModule, UserModule],
   controllers: [],
   providers: [PrismaService],
 })
